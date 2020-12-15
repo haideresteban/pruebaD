99e618a0a9b4b11236105c633afd0ecdea9fdafe
Haider Steban Rincon <herincon9@misena.edu.co>
2020-12-15 09:17:23 -0500

    mi primer commit

---
+++ PruebaConDiseno/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaConDiseno.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
